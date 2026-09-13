90a45ff749c61c6c5a245c4e862518abe7794f65
NeXora-404 <tariqulislam1437@gmail.com>
2026-09-13 20:18:18 +0600

    highlight technologies added to stack

--- src/components/TechnologyCard.jsx
+++ src/components/TechnologyCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TechnologyCard({ technology, added, onAdd }) {
   return (
-    <article className="technology-card">
+    <article className={`technology-card ${added ? "is-added" : ""}`}>
       <div className="card-top">
         <img
           className="technology-icon"
